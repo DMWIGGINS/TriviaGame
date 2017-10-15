767e3062b4b09f9bc226a2c8e3837e756e76621c
Doreen Wiggins <Wiggy77@aol.com>
2017-10-15 00:11:35 -0400

    foundation

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+//how to code the return of the radio button selected and tell the computer to tally results when done button is pressed.
+$(".done").on("click", function() {
+
+//can i make the return values of the correct answers different than the incorrect ones and then tally how many correct ones are returned? 
+
+//how to get answer choices to display correctly as a horizontal list? ok
+
+
+//only one choice can be selected for each question
+
+//Don't forget the timer! 
+
+
+
+$(":radio")
+
+
+
+
+
+
+
+
+
+
+}
