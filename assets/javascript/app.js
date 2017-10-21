aa085ae34ad05953fab29d5f3d8a432ef4740e12
Doreen Wiggins <Wiggy77@aol.com>
2017-10-21 01:03:05 -0400

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,17 @@
 $(document).ready(function () {
 
+    // set variables for timer
 
-    var number = 300;
+    var number = 150;
     var intervalId;
 
+    // create timer to countdown
 
     function timer() {
         intervalId = setInterval(decrement, 1000);
     }
 
+    // timer should stop at 0
     function decrement() {
         if (number === 0) {
             stop();
@@ -19,290 +22,302 @@ $(document).ready(function () {
         }
     }
 
+    // when timer gets to 0 it resets 
+
     function stop() {
         clearInterval(intervalId);
     }
 
+    // variables for correct answer counter, if question was answered correctly(boolean), unanswered counter, and incorrect counter
 
     var result = 0;
     var answer = 0;
     var unanswered = 0;
+    var incorrect = 0;
 
-    name = ["questionone", "questiontwo", "questionthree", "questionfour", "questionfive", "questionsix", "questionseven", "questioneight", "questionnine", "questionten", "questionbonus"];
-    correct = ["twodollars", "nothing", "jennifer", "jeans", "yellow", "tenderness", "dull", "frances", "banana", "conceive", "fire"];
-    
-    
+    // start the timer when the game is loaded
+
     timer();
 
-    //how to code the return of the radio button selected and tell the computer to tally results when done button is pressed.
-    $(".done").on("click", function () {
+    //    When done button is clicked ....
+
+    $("#done").on("click", function () {
+
+        // stop the timer
 
         stop();
+
+        // determine if question was answered - was a radio button selected?
 
         if ($('input:radio[name="questionone"]:checked').val()) {
 
             answer = (document.getElementById("twodollars").checked);
-            console.log(answer);
-            if (answer === true) {
-                alert("correct");
-                console.log("questionone");
-                result++;
-            } else {
-                alert("incorrect");
-                console.log("questionone");
 
+            //  Is it the correct answer? Add 1 to result
+
+            if (answer === true) {
+                result++;
+
+                // For wrong answer add 1 to incorrect
+
+            } else {
+                incorrect++;
             }
+
+            // if no answer selected, add 1 to unanswered
+
         } else {
             unanswered++;
-            console.log("didn't answer");
         }
+
+        // determine if question was answered - was a radio button selected?
+
         if ($('input:radio[name="questiontwo"]:checked').val()) {
 
             answer = (document.getElementById("nothing").checked);
-            console.log(answer);
+
+            //  Is it the correct answer? Add 1 to result
 
             if (answer === true) {
-                alert("correct");
-                console.log("questiontwo");
                 result++;
 
-            } else {
-                alert("incorrect");
-                console.log("questiontwo");
+                // For wrong answer add 1 to incorrect
 
+            } else {
+                incorrect++;
             }
+
+            // if no answer selected, add 1 to unanswered
+
         } else {
             unanswered++;
-            console.log("didn't answer");
         }
+
+            // determine if question was answered - was a radio button selected?
+
         if ($('input:radio[name="questionthree"]:checked').val()) {
 
             answer = (document.getElementById("jennifer").checked);
-            console.log(answer);
+
+            //  Is it the correct answer? Add 1 to result
+
             if (answer === true) {
-                alert("correct");
-                console.log("questionthree");
                 result++;
 
+                // For wrong answer add 1 to incorrect
+
             } else {
-                alert("incorrect");
-                console.log("questionthree");
+                incorrect++;
             }
+            // if no answer selected, add 1 to unanswered
+
         } else {
             unanswered++;
-            console.log("didn't answer");
         }
+        // determine if question was answered - was a radio button selected?
         if ($('input:radio[name="questionfour"]:checked').val()) {
 
             answer = (document.getElementById("jeans").checked);
-            console.log(answer);
+
+            //  Is it the correct answer? Add 1 to result
 
             if (answer === true) {
-                alert("correct");
-                console.log("questionfour");
                 result++;
 
-            } else {
-                alert("incorrect");
-                console.log("questionfour");
+                // For wrong answer add 1 to incorrect
 
+            } else {
+                incorrect++;
             }
+
+            // if no answer selected, add 1 to unanswered
+
         } else {
             unanswered++;
-            console.log("didn't answer");
         }
+
+        // determine if question was answered - was a radio button selected?
 
         if ($('input:radio[name="questionfive"]:checked').val()) {
 
             answer = (document.getElementById("yellow").checked);
-            console.log(answer);
+
+            //  Is it the correct answer? Add 1 to result
 
             if (answer === true) {
-                alert("correct");
-                console.log("questionfive");
                 result++;
 
-            } else {
-                alert("incorrect");
-                console.log("questionfive");
+                // For wrong answer add 1 to incorrect
 
+            } else {
+                incorrect++;
             }
+
+            // if no answer selected, add 1 to unanswered
+
         } else {
             unanswered++;
-            console.log("didn't answer");
         }
+        // determine if question was answered - was a radio button selected?
+
         if ($('input:radio[name="questionsix"]:checked').val()) {
 
             answer = (document.getElementById("tenderness").checked);
-            console.log(answer);
+
+            //  Is it the correct answer? Add 1 to result
 
             if (answer === true) {
-                alert("correct");
-                console.log("questionsix");
                 result++;
 
-            } else {
-                alert("incorrect");
-                console.log("questionsix");
+                // For wrong answer add 1 to incorrect
 
+            } else {
+                incorrect++;
             }
+
+            // if no answer selected, add 1 to unanswered
+
         } else {
             unanswered++;
-            console.log("didn't answer");
         }
+
+        // determine if question was answered - was a radio button selected?
 
         if ($('input:radio[name="questionseven"]:checked').val()) {
 
             answer = (document.getElementById("dull").checked);
-            console.log(answer);
+
+            //  Is it the correct answer? Add 1 to result
 
             if (answer === true) {
-                alert("correct");
-                console.log("questionseven");
                 result++;
+                // For wrong answer add 1 to incorrect
 
             } else {
-                alert("incorrect");
-                console.log("questionseven");
-
+                incorrect++;
             }
+            // if no answer selected, add 1 to unanswered
+
         } else {
             unanswered++;
-            console.log("didn't answer");
         }
+
+        // determine if question was answered - was a radio button selected?
+
         if ($('input:radio[name="questioneight"]:checked').val()) {
 
             answer = (document.getElementById("frances").checked);
-            console.log(answer);
+
+            //  Is it the correct answer? Add 1 to result
 
             if (answer === true) {
-                alert("correct");
-                console.log("questioneight");
                 result++;
+                // For wrong answer add 1 to incorrect
 
             } else {
-                alert("incorrect");
-                console.log("questioneight");
-
+                incorrect++;
             }
+            // if no answer selected, add 1 to unanswered
+
         } else {
             unanswered++;
-            console.log("didn't answer");
         }
+        // determine if question was answered - was a radio button selected?
+
         if ($('input:radio[name="questionnine"]:checked').val()) {
 
             answer = (document.getElementById("banana").checked);
-            console.log(answer);
+            //  Is it the correct answer? Add 1 to result
 
             if (answer === true) {
-                alert("correct");
-                console.log("questionnine");
                 result++;
+                // For wrong answer add 1 to incorrect
 
             } else {
-                alert("incorrect");
-                console.log("questionnine");
+                incorrect++;
 
             }
+            // if no answer selected, add 1 to unanswered
+
         } else {
             unanswered++;
-            console.log("didn't answer");
         }
+        // determine if question was answered - was a radio button selected?
+
         if ($('input:radio[name="questionten"]:checked').val()) {
 
             answer = (document.getElementById("conceive").checked);
-            console.log(answer);
+            //  Is it the correct answer? Add 1 to result
 
             if (answer === true) {
-                alert("correct");
-                console.log("questionten");
                 result++;
 
+                // For wrong answer add 1 to incorrect
+
             } else {
-                alert("incorrect");
-                console.log("questionten");
+                incorrect++;
 
             }
+            // if no answer selected, add 1 to unanswered
+
         } else {
             unanswered++;
-            console.log("didn't answer");
         }
+        // determine if question was answered - was a radio button selected?
+
         if ($('input:radio[name="questionbonus"]:checked').val()) {
 
             answer = (document.getElementById("fire").checked);
-            console.log(answer);
+            //  Is it the correct answer? Add 1 to result
 
             if (answer === true) {
-                alert("correct");
-                console.log("questionbonus");
                 result++;
+                // For wrong answer add 1 to incorrect
 
             } else {
-                alert("incorrect");
-                console.log("questionbonus");
-
+                incorrect++;
             }
+            // if no answer selected, add 1 to unanswered
+
         } else {
             unanswered++;
-            console.log("didn't answer");
         }
+        // determine how many unanswered questions
 
-        $("#result").text(result);
+        unanswered = 11 - (result + incorrect);
+
+        // create results page to replace original question page
+
+        $("#newpage").html('<div id="new"></div>');
+
+        // style for results page
+
+        $("#new").css({
+            'background-color': 'rgb(216, 55, 55)',
+            'height': '320px',
+            'width': '650px',
+            'margin': 'auto',
+            'margin-top': '100px',
+            'border-style': 'solid',
+            'border-color': 'black',
+            'padding-left': '20px',
+            'position': 'relative',
+        });
+
+        // Adding stats and image to results page
+
+        $("#new").html("<h1>Results</h1>");
+        $("#new").append("<h2>Correct: " + result + "</h2>");
+        $("#new").append("<h2>Incorrect: " + incorrect + "</h2>");
+        $("#new").append("<h2>Unanswered: " + unanswered + "</h2>");
+        $("#new").append("<img id='duckie2' class='inline' src='assets/images/duckie2.jpg'>");
+
+        // styling for image
+
+        $("#duckie2").css({
+            'position': 'absolute',
+            'top': '20px',
+            'right': '80px',
+        });
     });
-})
-//returning the same true or false responses depending on what the first question is.
-// So if the first question is answer === true then all questions are returning as true. 
-// Likewise is answer === false for question one then all are returning false.  
-// I changed the id's of each question so they are not the same-seems to have fixed the issue but not efficient. 
-
-
-
-
-// function myFunction() {
-//     var x = document.getElementById("myRadio").checked;
-//     document.getElementById("demo").innerHTML = x;
-
-
-
-// for (i = 0; i < "questionone".length; i++) {
-//    val = (document.getElementById("triviaquestions"), "questionone");
-//     alert(val);
-// }
-
-
-
-
-
-/* <button id="button-minus" class="btn btn-danger operator" value="plus"><h1>&minus;</h1></button>
- if (operator === "plus") {
-    result = firstNumber + secondNumber; */
-
-
-// $('input:radio[name="genderS"]:checked').val()
-// $( "input[name=gender]:radio" )
-// or
-// $("input:radio")
-
-// $( "input[radio][questionone]:checked" ).val();
-
-// document.querySelector('input[name="genderS"]:checked').value;
-
-
-// if ("questionone" === "correct") {
-//     rightanswers++;
-//     cosole.log(rightanswers);
-
-
-//can i make the return values of the correct answers different than the incorrect ones and then tally how many correct ones are returned? 
-
-//how to get answer choices to display correctly as a horizontal list? ok
-
-
-//only one choice can be selected for each question ok
-
-//Don't forget the timer! 
-
-
-
-
-// * <input type="radio" onclick="if(this.checked){myFunction()}">
+});
