7121114e2bdad5097717266e09b8340f96ef45f1
Doreen Wiggins <Wiggy77@aol.com>
2025-02-25 16:03:03 -0500

    creating JSON object for questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     // set variables for timer
 
-    var number = 150;
+    var number = 10;
     var intervalId;
 
     // create timer to countdown
@@ -14,19 +14,126 @@ $(document).ready(function () {
     // timer should stop at 0
     function decrement() {
         if (number === 0) {
+            $("#timeleft").text(number);  
+            setTimeout(function() {
+                alert("Sorry you are out of time!");
+            }, 100);
             stop();
-            alert("Sorry, you are out of time!");
         } else {
             $("#timeleft").text(number);
             number--;
         }
+        
     }
+
+    
 
     // when timer gets to 0 it resets 
 
     function stop() {
         clearInterval(intervalId);
     }
+
+    // storing the questions and answers in an object so program is easy to reuse with new questions
+
+    const Trivia = {
+                    QuestionOne:{Heading:"Better Off Dead",
+                                  QuestionText: "What is the paperboy stalking Lane for?",
+                                Answers: {
+                                    Correct:"His two dollars", 
+                                    InCorrect: [
+                                                "His skiis", 
+                                                "His bike", 
+                                                "His apple"]
+                                            }
+                                        },
+                    QuestionTwo:"Why is the 'crazy girl' in detention?",
+                                Answers: {
+                                    Correct:"She had nothing better to do on a Saturday",
+                                    InCorrect: [
+                                               "She tripped a popular kid in the cafeteria" ,
+                                               "She is a mole for the teachers",
+                                               "She was caught selling drugs"
+                                              ]
+                                             },
+                    QuestionThree:"Who played Ferris' cranky sister?",
+                                Answers: {
+                                    Correct:"Jennifer Grey", 
+                                    InCorrect: [
+                                                "Mia Sara", 
+                                                "Demi Moore", 
+                                                "Phoebe Cates"]
+                                            },
+                    QuestionFour:"What did Gary and Wyatt wear when showering with the woman they created??",
+                                Answers: {
+                                    Correct:"Jeans", 
+                                    InCorrect: [
+                                                "Sexy speedos", 
+                                                "Flotation Devices", 
+                                                "Smiles"]
+                                            },
+                    QuestionFive:"What color car did Daniel choose as a birthday present from Mr. Miyagi?",
+                                Answers: {
+                                    Correct:"Yellow", 
+                                    InCorrect: [
+                                                "White", 
+                                                "Light Blue", 
+                                                "Silver"]
+                                            },
+                    QuestionSix:"What song did Duckie lip-sync his heart out to at the record store?",
+                                Answers: {
+                                    Correct:"Try a Little Tenderness", 
+                                    InCorrect: [
+                                                "Let's Get it On", 
+                                                "Sexual Healing", 
+                                                "Superfreak"]
+                                            },
+                    QuestionSeven:"What does 'All work and no play' make Jack?",
+                                Answers: {
+                                    Correct:"A dull boy", 
+                                    InCorrect: [
+                                                "A psycho killer", 
+                                                "Really boring", 
+                                                "A weirdo"]
+                                            },
+                    QuestionEight:"What is Baby's real name?",
+                                Answers: {
+                                    Correct:"Frances", 
+                                    InCorrect: [
+                                                "Alma", 
+                                                "Agnes", 
+                                                "Ingrid"]
+                                            },
+                    QuestionNine:"When the ghosts temporarily possessed the family during dinner, what song did they make them sing and dance to?",
+                                Answers: {
+                                    Correct:"The Banana Boat Song", 
+                                    InCorrect: [
+                                                "The Monster Mash", 
+                                                "Purple People Eater", 
+                                                "Thriller"]
+                                            },
+                    QuestionTen:"What word did the clergyman with the speech impediment love to say?",
+                                Answers: {
+                                    Correct:"Inconceivable", 
+                                    InCorrect: [
+                                                "Unbelievable", 
+                                                "Irretrievable", 
+                                                "Irresponsible"]
+                                            },
+                    QuestionBonus: "Which of the following movies did NOT star Molly Ringwald",
+                                Answers: {
+                                 Correct:"St Elmo's Fire", 
+                                 InCorrect: [
+                                                "Sixteen Candles", 
+                                                "The Breakfast Club", 
+                                                "Pretty in Pink"]
+                                             },
+                                        };
+
+  
+
+   
+   $("#testing").text(Trivia.QuestionOne.Answers.InCorrect[0])
 
     // variables for correct answer counter, if question was answered correctly(boolean), unanswered counter, and incorrect counter
 
